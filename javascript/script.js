c2f87246e2f146e097f3f914707340d30c75be0d
Sulav <sul4v2@gmail.com>
2021-06-15 23:47:37 +0545

    Added the dark mode feature, and will work on the font-size buttons tomorrow.

--- javascript/script.js
+++ javascript/script.js
@@ -4,13 +4,21 @@ var lineHeight = document.getElementById("line-height-btn");
 var leftAlign = document.getElementById('left-align');
 var centerAlign = document.getElementById('center-align');
 var rightAlign = document.getElementById('right-align');
-// var divs = document.querySelectorAll('div');
+
+var fontDown = document.querySelector(".size-minus");
+var fontUp = document.querySelector(".size-plus");
+
+var darkModeButton = document.querySelector("#darkmode-button");
+let checkbox = document.querySelector('input[name=theme]');
+
 
 // Font Size
-var select = document.getElementById("selection");
-select.addEventListener("change", function() {
-    textArea.style.fontSize = `${this.value}px`;
-});
+// var select = document.getElementById("selection");
+// select.addEventListener("change", function() {
+//     textArea.style.fontSize = `${this.value}px`;
+// });
+
+fontUp.addEventListener("click", () => console.log(textArea.style.fontSize));
 
 // Line Height
 lineHeight.addEventListener("click", () => {
@@ -23,3 +31,52 @@ lineHeight.addEventListener("click", () => {
 leftAlign.addEventListener('click', () => textArea.style.textAlign = 'left')
 centerAlign.addEventListener('click', () => textArea.style.textAlign = 'center')
 rightAlign.addEventListener('click', () => textArea.style.textAlign = 'right')
+
+
+
+/////////////////////////  This is for the dark mode ////////////////////////////
+
+// This is for transition
+
+let trans = () => {
+  document.documentElement.classList.add('transition');
+  window.setTimeout(() => {
+      document.documentElement.classList.remove('transition')
+  }, 300)
+}
+
+// Button click action
+
+darkModeButton.addEventListener('click', () =>{
+  if(checkbox.checked){
+      checkbox.checked = false;
+      localStorage.setItem("checkbox", 'false');
+
+      document.documentElement.setAttribute('data-theme', 'light');
+      trans();
+  } else {
+      checkbox.checked = true;
+      localStorage.setItem("checkbox", true);
+      
+      document.documentElement.setAttribute('data-theme', 'dark');
+      trans();
+  }
+})
+
+// Checking if the user already has theme selected in the localstorage
+
+function checkLocalStorage() {
+  if(localStorage.getItem('checkbox') == null){
+      //pass
+  } else {
+      let checked = JSON.parse(localStorage.getItem('checkbox'));
+      if(checked == true){
+          document.documentElement.setAttribute('data-theme', 'dark');
+          checkbox.checked = true;
+      }
+  }
+}
+
+checkLocalStorage();
+
+//////////////////////////// Upto here dark mode ///////////////////////////
